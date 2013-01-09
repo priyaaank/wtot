e130126ab667c25e90b17c937833be95bd955be7
Priyank Gupta <priyaaank@gmail.com>
2013-01-09 23:24:30 +0530

    Adding provision to keep api keys locally for development

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 
+heroku_env = File.join('./.heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
 end
